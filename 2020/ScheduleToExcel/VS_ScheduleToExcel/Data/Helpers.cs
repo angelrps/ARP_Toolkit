eb1f562b70f157c6f593de3f252cfcd28adcd64a
angelrps <angelruizpeinado@gmail.com>
2020-04-14 18:07:34 +0200

    .\ScheduleToExcel\ added

---
+++ 2020/ScheduleToExcel/VS_ScheduleToExcel/Data/Helpers.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.ApplicationServices;
+
+namespace Data
+{
+    // method to replace illegal characters
+    class Helpers
+    {
+        // remove invalida characters from schedule name
+        public static string ReplaceIllegalCharaters (string stringWithIllegalChar)
+        {
+            char[] badChars = Path.GetInvalidFileNameChars();
+            string updated = stringWithIllegalChar;
+            foreach (char c in badChars)
+            {
+                updated = updated.Replace(c.ToString(), "_");
+            }
+            return updated;
+        }
+
+        // getFileMajorVersion
+        private int GetMajorVersionFile (string p)
+        {
+            // return value
+            int m_val = 0;
+            // check for file
+            if (System.IO.File.Exists(p))
+            {
+                try
+                {
+                    m_val = System.Diagnostics.FileVersionInfo.GetVersionInfo(p).FileMajorPart;
+                }
+                catch (Exception)
+                {
+                }
+            }
+            return m_val;
+        }
+        // get office installed version
+
+        public static bool CheckFileExists (string excelPaths_FilePath)
+        {
+            bool fileExists = true;
+            if (!File.Exists(excelPaths_FilePath))
+            {                
+                fileExists = false;
+            }
+            return fileExists;
+        }
+
+        public static bool CheckExcelInstall (string excelPaths_FilePath)
+        {
+            bool excelPathExists = false;
+
+            string thisLine = "";
+            using (StreamReader reader = new StreamReader(excelPaths_FilePath))
+            {
+                while (thisLine != null)
+                {
+                    thisLine = reader.ReadLine();
+                    if (File.Exists(thisLine))
+                    {
+                        excelPathExists = true;
+                        break;
+                    }
+                }
+            }
+            return excelPathExists;
+        }
+
+        public static List<View> GetViewSelection(Document doc, ICollection<ElementId> selIds)
+        {
+            List<View> validViews = new List<View>();
+
+            if (0 != selIds.Count)
+            {
+                // check if the selection are view schedules
+                foreach (ElementId eId in selIds)
+                {
+                    Element elem = doc.GetElement(eId);
+
+                    try // try cast to View. If ViewType == schedule, add to list.
+                    {
+                        View v = elem as View;
+                        if (v.ViewType == ViewType.Schedule)
+                        {
+                            validViews.Add(v);
+                        }
+                    }
+                    catch (Exception)
+                    {
+                    }
+                }
+            }
+            return validViews;
+        }
+
+        public static View GetActiveViewSchedule(Document doc)
+        {
+            if (doc.ActiveView.ViewType == ViewType.Schedule)
+            {
+                return doc.ActiveView;
+            }
+            return null;
+        }
+
+        public static List<View> GetSelectedSchedules(Document doc, ICollection<ElementId> selIds)
+        {
+            List<View> scheduleList = new List<View>();
+
+            foreach (View v in GetViewSelection(doc, selIds))
+            {
+                scheduleList.Add(v);
+            }
+            if (doc.ActiveView.ViewType == ViewType.Schedule)
+            {
+                scheduleList.Add(doc.ActiveView);
+            }           
+
+            return scheduleList;
+        }
+
+        public static void ScheduleToExcelAnalytics(Document doc, Application app,
+                                                    string UseTimeElapseS,
+                                                    string ExecTimeElapseS,
+                                                    int Ana_NoOfExports)
+        {
+            #region INFORMATION VARIABLES
+            string date = DateTime.Now.ToString("yyyy.MM.dd");
+            string time = DateTime.Now.ToString("HH:mm:ss");
+
+            string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+            string appName = Assembly.GetExecutingAssembly().GetName().Name;
+            string fileName = doc.Title;
+
+            string projectNumber = doc.ProjectInformation.Number;
+            string projectName = doc.ProjectInformation.Name;
+            string isInCloud = doc.IsModelInCloud.ToString();
+
+            string subVersionNumber = app.SubVersionNumber;
+            string buildNumber = app.VersionBuild;
+            string pcNumber = System.Environment.MachineName;
+
+            string fullString = string.Format(@"{0};{1};{2};{3};{4};{5};{6};{7};{8};{9};{10};{11};{12};{13}",
+                                                date, time, userName, appName,
+                                                fileName, projectNumber, projectName, isInCloud, subVersionNumber, buildNumber, pcNumber,
+                                                UseTimeElapseS, ExecTimeElapseS, Ana_NoOfExports);
+            #endregion
+
+            string csvName = "Analytics_ScheduleToExcel" + ".csv";
+
+            string filePath = @"D:\ANGEL\BIM\07-Macros and Plugins\ARP_Tools\z_Resources";
+            string fullPath = Path.Combine(filePath, csvName);
+
+            try
+            {
+                using (StreamWriter sw = File.AppendText(fullPath))
+                {
+                    sw.WriteLine(fullString);
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
---
+++ 2020/ScheduleToExcel/VS_ScheduleToExcel/Entry/CmdMain.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Microsoft.Office.Interop.Excel;
+using Autodesk.Revit.ApplicationServices;
+using ArpUtilities;
+using System.Diagnostics;
+
+namespace Entry
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    public class CmdMain : IExternalCommand
+    {
+        static AddInId appId = new AddInId(new Guid("E8232172-2898-410F-9535-364C2D46445F"));
+
+        #region Variables for analytics
+        private static readonly Stopwatch useTime = new Stopwatch();        // total time user has the application open and it does actually make a transaction
+
+        public static string UseTimeElapseS { get; set; } = "";
+        public static string ExecTimeElapseS { get; set; } = "";    // for this application Use Time will be the same as Execution Time
+        public static int Ana_NoOfExports { get; set; } = 0;        // number of schedules exported
+        #endregion
+
+        public Result Execute (ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            #region variables
+            UIApplication uiapp = commandData.Application;
+            Autodesk.Revit.ApplicationServices.Application app = uiapp.Application;
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            // element selection
+            ICollection<ElementId> selIds = uidoc.Selection.GetElementIds();
+
+            // excel export settings
+            ViewScheduleExportOptions export_options = new ViewScheduleExportOptions();
+
+            // paths to check Excel install                  
+            string excelPaths_FileName = "OpenInExcel_UserPaths.txt";
+            string assemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string excelPaths_FilePath = Path.Combine(assemblyPath, excelPaths_FileName);
+
+            // paths to export schedule
+            string temp_folder = Path.GetTempPath();
+            #endregion
+            
+            // check if file exists
+            if (!Data.Helpers.CheckFileExists(excelPaths_FilePath))
+            {
+                TaskDialog.Show("Schedule To Excel", "Could not find the File: " + "\n"
+                                                      + excelPaths_FileName + " in "
+                                                      + assemblyPath + "\n" + "Contact the BIM Department for help.");
+                return Result.Cancelled;
+            }
+
+            // check if excel is installed in usual paths
+            if (!Data.Helpers.CheckExcelInstall(excelPaths_FilePath))
+            {
+                TaskDialog.Show("Schedule To Excel", "Could not find Excel installed in the usual paths." + "\n"
+                                                     + "Please add you Excel path to OpenInExcel_UserPaths.txt and try again." + "\n"
+                                                     + "Contact the BIM Department for help.");
+                return Result.Cancelled;
+            }
+
+            // if there are not schedules selected or open throw a message
+            if (!Data.Helpers.GetSelectedSchedules(doc, selIds).Any())
+            {
+                TaskDialog.Show("Schedule To Excel", "You must have a schedule open or selected in the Project Browser.");
+                return Result.Cancelled;
+            }
+
+            // go ahead and export the selected schedules
+            if (Data.Helpers.GetSelectedSchedules(doc, selIds).Any())
+            {
+                // start counters. For this application Use Time and Execution time will be the same.
+                useTime.Start();
+
+                Random randomNumber = new Random();
+
+                // create excel name with schedule name + random number
+                foreach (View v in Data.Helpers.GetSelectedSchedules(doc, selIds))
+                {
+                    Ana_NoOfExports += 1;   // count number of schedules exported
+
+                    string scheduleName = Data.Helpers.ReplaceIllegalCharaters(v.Name);
+                    string txtFileName = scheduleName + randomNumber.Next(100) + ".txt";
+                    string scheduleExportPath = Path.Combine(temp_folder, txtFileName);
+
+                    // export schedule to .txt
+                    ViewSchedule viewSchedule = v as ViewSchedule;
+                    viewSchedule.Export(temp_folder, txtFileName, export_options);
+
+                    // open .txt schedule with excel
+                    Microsoft.Office.Interop.Excel.Application excelApp = new Microsoft.Office.Interop.Excel.Application();
+                    excelApp.Visible = true; // make the running excel instance visible
+                    Workbook wb = excelApp.Workbooks.Open(scheduleExportPath);
+                }
+                useTime.Stop();
+                UseTimeElapseS = useTime.Elapsed.Seconds.ToString();
+                ExecTimeElapseS = UseTimeElapseS;
+            }
+            try
+            {
+                Utilities.GetAnalyticsCSV(doc, app);
+                Data.Helpers.ScheduleToExcelAnalytics(doc, app, UseTimeElapseS, ExecTimeElapseS, Ana_NoOfExports);
+            }
+            catch (Exception)
+            {
+            }
+            return Result.Succeeded;
+        }
+    }
+}
