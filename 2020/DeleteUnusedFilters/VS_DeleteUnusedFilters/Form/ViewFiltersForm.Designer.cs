03396f394e483a6c073afde7da20c5c0b9492313
angelrps <angelruizpeinado@gmail.com>
2020-04-16 16:12:02 +0200

    UI interface updated

--- 2020/DeleteUnusedFilters/VS_DeleteUnusedFilters/Form/ViewFiltersForm.Designer.cs
+++ 2020/DeleteUnusedFilters/VS_DeleteUnusedFilters/Form/ViewFiltersForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ViewFiltersForm));
             this.clbViewFilters = new System.Windows.Forms.CheckedListBox();
             this.lbMessage1 = new System.Windows.Forms.Label();
@@ -37,6 +38,15 @@
             this.btnNone = new System.Windows.Forms.Button();
             this.lbMessage2 = new System.Windows.Forms.Label();
             this.lbTotalItems = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.LblLineSeparator = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.CheckAll = new System.Windows.Forms.ToolTip(this.components);
+            this.UncheckAll = new System.Windows.Forms.ToolTip(this.components);
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // clbViewFilters
@@ -47,108 +57,188 @@
             this.clbViewFilters.CheckOnClick = true;
             this.clbViewFilters.FormattingEnabled = true;
             this.clbViewFilters.HorizontalScrollbar = true;
-            this.clbViewFilters.Location = new System.Drawing.Point(12, 27);
+            this.clbViewFilters.Location = new System.Drawing.Point(20, 97);
+            this.clbViewFilters.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.clbViewFilters.Name = "clbViewFilters";
-            this.clbViewFilters.Size = new System.Drawing.Size(226, 184);
+            this.clbViewFilters.Size = new System.Drawing.Size(302, 284);
             this.clbViewFilters.TabIndex = 0;
             this.clbViewFilters.SelectedIndexChanged += new System.EventHandler(this.clbViewFilters_SelectedIndexChanged);
             // 
             // lbMessage1
             // 
             this.lbMessage1.AutoSize = true;
-            this.lbMessage1.Location = new System.Drawing.Point(9, 9);
+            this.lbMessage1.Location = new System.Drawing.Point(20, 70);
             this.lbMessage1.Name = "lbMessage1";
-            this.lbMessage1.Size = new System.Drawing.Size(176, 13);
+            this.lbMessage1.Size = new System.Drawing.Size(227, 19);
             this.lbMessage1.TabIndex = 1;
             this.lbMessage1.Text = "Select unused filters to be removed:";
             // 
             // btnOK
             // 
-            this.btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.btnOK.Location = new System.Drawing.Point(12, 242);
+            this.btnOK.BackColor = System.Drawing.Color.Transparent;
+            this.btnOK.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnOK.FlatAppearance.BorderSize = 0;
+            this.btnOK.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(172)))), ((int)(((byte)(0)))));
+            this.btnOK.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOK.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOK.ForeColor = System.Drawing.Color.White;
+            this.btnOK.Location = new System.Drawing.Point(0, 0);
+            this.btnOK.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnOK.Name = "btnOK";
-            this.btnOK.Size = new System.Drawing.Size(110, 30);
+            this.btnOK.Size = new System.Drawing.Size(175, 78);
             this.btnOK.TabIndex = 2;
-            this.btnOK.Text = "OK";
-            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Text = "Purge";
+            this.btnOK.UseVisualStyleBackColor = false;
             this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
             // 
             // btnCancel
             // 
-            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.BackColor = System.Drawing.Color.Transparent;
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancel.Location = new System.Drawing.Point(128, 242);
+            this.btnCancel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnCancel.FlatAppearance.BorderSize = 0;
+            this.btnCancel.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(172)))), ((int)(((byte)(0)))));
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.ForeColor = System.Drawing.Color.White;
+            this.btnCancel.Location = new System.Drawing.Point(199, 0);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(110, 30);
+            this.btnCancel.Size = new System.Drawing.Size(175, 78);
             this.btnCancel.TabIndex = 3;
-            this.btnCancel.Text = "Cancel";
-            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Text = "Close";
+            this.btnCancel.UseVisualStyleBackColor = false;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
             // btnAll
             // 
             this.btnAll.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnAll.Location = new System.Drawing.Point(246, 27);
+            this.btnAll.BackColor = System.Drawing.Color.Transparent;
+            this.btnAll.FlatAppearance.BorderSize = 0;
+            this.btnAll.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(172)))), ((int)(((byte)(0)))));
+            this.btnAll.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAll.Image = ((System.Drawing.Image)(resources.GetObject("btnAll.Image")));
+            this.btnAll.Location = new System.Drawing.Point(322, 97);
+            this.btnAll.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnAll.Name = "btnAll";
-            this.btnAll.Size = new System.Drawing.Size(75, 23);
+            this.btnAll.Size = new System.Drawing.Size(50, 35);
             this.btnAll.TabIndex = 4;
-            this.btnAll.Text = "Check All";
-            this.btnAll.UseVisualStyleBackColor = true;
+            this.CheckAll.SetToolTip(this.btnAll, "Check All");
+            this.btnAll.UseVisualStyleBackColor = false;
             this.btnAll.Click += new System.EventHandler(this.btnAll_Click);
             // 
             // btnNone
             // 
             this.btnNone.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnNone.Location = new System.Drawing.Point(246, 56);
+            this.btnNone.BackColor = System.Drawing.Color.Transparent;
+            this.btnNone.FlatAppearance.BorderSize = 0;
+            this.btnNone.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(172)))), ((int)(((byte)(0)))));
+            this.btnNone.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNone.Image = ((System.Drawing.Image)(resources.GetObject("btnNone.Image")));
+            this.btnNone.Location = new System.Drawing.Point(322, 138);
+            this.btnNone.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnNone.Name = "btnNone";
-            this.btnNone.Size = new System.Drawing.Size(75, 23);
+            this.btnNone.Size = new System.Drawing.Size(50, 35);
             this.btnNone.TabIndex = 5;
-            this.btnNone.Text = "Check None";
-            this.btnNone.UseVisualStyleBackColor = true;
+            this.UncheckAll.SetToolTip(this.btnNone, "Uncheck All");
+            this.btnNone.UseVisualStyleBackColor = false;
             this.btnNone.Click += new System.EventHandler(this.btnNone_Click);
             // 
             // lbMessage2
             // 
             this.lbMessage2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.lbMessage2.AutoSize = true;
-            this.lbMessage2.Location = new System.Drawing.Point(9, 214);
+            this.lbMessage2.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMessage2.Location = new System.Drawing.Point(20, 400);
             this.lbMessage2.Name = "lbMessage2";
-            this.lbMessage2.Size = new System.Drawing.Size(107, 13);
+            this.lbMessage2.Size = new System.Drawing.Size(111, 19);
             this.lbMessage2.TabIndex = 6;
-            this.lbMessage2.Text = "Total Selected Items:";
+            this.lbMessage2.Text = "Items to Purge:";
             // 
             // lbTotalItems
             // 
             this.lbTotalItems.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.lbTotalItems.Location = new System.Drawing.Point(162, 214);
+            this.lbTotalItems.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTotalItems.Location = new System.Drawing.Point(131, 400);
             this.lbTotalItems.Name = "lbTotalItems";
-            this.lbTotalItems.Size = new System.Drawing.Size(76, 13);
+            this.lbTotalItems.Size = new System.Drawing.Size(50, 17);
             this.lbTotalItems.TabIndex = 7;
             this.lbTotalItems.Text = "0";
             this.lbTotalItems.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.lbTotalItems.Click += new System.EventHandler(this.lbTotalItems_Click);
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Gray;
+            this.panel1.Controls.Add(this.btnCancel);
+            this.panel1.Controls.Add(this.btnOK);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 436);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(374, 78);
+            this.panel1.TabIndex = 8;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel3.Controls.Add(this.LblLineSeparator);
+            this.panel3.Controls.Add(this.pictureBox1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(374, 50);
+            this.panel3.TabIndex = 32;
+            // 
+            // LblLineSeparator
+            // 
+            this.LblLineSeparator.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LblLineSeparator.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.LblLineSeparator.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LblLineSeparator.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.LblLineSeparator.Location = new System.Drawing.Point(10, 40);
+            this.LblLineSeparator.Name = "LblLineSeparator";
+            this.LblLineSeparator.Size = new System.Drawing.Size(350, 2);
+            this.LblLineSeparator.TabIndex = 32;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(10, 10);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(150, 25);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
             // ViewFiltersForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
             this.CancelButton = this.btnCancel;
-            this.ClientSize = new System.Drawing.Size(329, 281);
+            this.ClientSize = new System.Drawing.Size(374, 514);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.lbTotalItems);
             this.Controls.Add(this.lbMessage2);
             this.Controls.Add(this.btnNone);
             this.Controls.Add(this.btnAll);
-            this.Controls.Add(this.btnCancel);
-            this.Controls.Add(this.btnOK);
             this.Controls.Add(this.lbMessage1);
             this.Controls.Add(this.clbViewFilters);
+            this.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MaximumSize = new System.Drawing.Size(600, 600);
-            this.MinimumSize = new System.Drawing.Size(345, 320);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximumSize = new System.Drawing.Size(697, 773);
+            this.MinimumSize = new System.Drawing.Size(390, 553);
             this.Name = "ViewFiltersForm";
+            this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Delete Filters";
+            this.Text = "Purge Filters";
             this.Load += new System.EventHandler(this.ViewFiltersForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -164,5 +254,11 @@
         private System.Windows.Forms.Button btnNone;
         private System.Windows.Forms.Label lbMessage2;
         private System.Windows.Forms.Label lbTotalItems;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label LblLineSeparator;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolTip CheckAll;
+        private System.Windows.Forms.ToolTip UncheckAll;
     }
 }
