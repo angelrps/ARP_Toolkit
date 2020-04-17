a2a79482fd88c08554f84e37a70962fde23cb979
angelrps <angelruizpeinado@gmail.com>
2020-04-17 08:23:19 +0200

    form size fixed

--- 2020/DeleteUnusedTextNoteTypes/VS_DeleteUnusedTextNoteTypes/Form/TextNotesForm.Designer.cs
+++ 2020/DeleteUnusedTextNoteTypes/VS_DeleteUnusedTextNoteTypes/Form/TextNotesForm.Designer.cs
@@ -296,6 +296,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MinimumSize = new System.Drawing.Size(618, 600);
             this.Name = "TextNotesForm";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
