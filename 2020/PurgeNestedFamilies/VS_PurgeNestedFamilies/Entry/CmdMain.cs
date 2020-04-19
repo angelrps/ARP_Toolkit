dd598a53908477c06e1f8d2eb84ac1e0c0d3b86a
angelrps <angelruizpeinado@gmail.com>
2020-04-19 08:13:21 +0200

    WIP tree view results

--- 2020/PurgeNestedFamilies/VS_PurgeNestedFamilies/Entry/CmdMain.cs
+++ 2020/PurgeNestedFamilies/VS_PurgeNestedFamilies/Entry/CmdMain.cs
@@ -9,6 +9,7 @@ using System.Windows.Media.Imaging;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.ApplicationServices;
+using ArpUtilities;
 
 namespace Entry
 {
@@ -16,6 +17,11 @@ namespace Entry
 
     public class CmdMain : IExternalCommand
     {
+        public static List<string> lt_purgedFamilies = new List<string>();
+        public static List<string> lt_purgedNestedFam = new List<string>();
+        public static List<string> lt_purgedNestedTypes = new List<string>();
+        public static Dictionary<string, Dictionary<string, List<string>>> DictResults = new Dictionary<string, Dictionary<string, List<string>>>();
+
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
             UIApplication uiapp = commandData.Application;
@@ -24,12 +30,53 @@ namespace Entry
             Document doc = uidoc.Document;
 
             #region variables declaration
-            List<int> categoriesToPurge = Data.Helpers.CategoriesToPurge();
-            List<Family> editableFamilies = Data.Helpers.GetEditableFamilies(doc);
-            List<Family> familiesToPurge = new List<Family>();   
+            List<int> categoriesToPurge = new List<int>();
+            List<Family> familiesToPurge = new List<Family>();
             string st_purgedNestedFam = "";
             string st_purgedFamilies = "";
             string st_purgedNestedTypes = "";
+            #endregion
+
+            #region List of loadable categories which families will be purged
+
+            categoriesToPurge.Add((int)BuiltInCategory.OST_CableTrayFitting);
+            categoriesToPurge.Add((int)BuiltInCategory.OST_Casework);
+            categoriesToPurge.Add((int)BuiltInCategory.OST_Columns);
+            categoriesToPurge.Add((int)BuiltInCategory.OST_CommunicationDevices);
+            categoriesToPurge.Add((int)BuiltInCategory.OST_ConduitFitting);
+            categoriesToPurge.Add((int)BuiltInCategory.OST_DataDevices);
+            categoriesToPurge.Add((int)BuiltInCategory.OST_Doors);
+            categoriesToPurge.Add((int)BuiltInCategory.OST_DuctAccessory);
+            categoriesToPurge.Add((int)BuiltInCategory.OST_DuctFitting);
+            categoriesToPurge.Add((int)BuiltInCategory.OST_ElectricalEquipment);
+            categoriesToPurge.Add((int)BuiltInCategory.OST_ElectricalFixtures);
+            categoriesToPurge.Add((int)BuiltInCategory.OST_Entourage);
+            categoriesToPurge.Add((int)BuiltInCategory.OST_FireAlarmDevices);
+            categoriesToPurge.Add((int)BuiltInCategory.OST_Furniture);
+            categoriesToPurge.Add((int)BuiltInCategory.OST_FurnitureSystems);
+            categoriesToPurge.Add((int)BuiltInCategory.OST_GenericModel);
+            categoriesToPurge.Add((int)BuiltInCategory.OST_LightingDevices);
+            categoriesToPurge.Add((int)BuiltInCategory.OST_LightingFixtures);
+            categoriesToPurge.Add((int)BuiltInCategory.OST_Mass);
+            categoriesToPurge.Add((int)BuiltInCategory.OST_MechanicalEquipment);
+            categoriesToPurge.Add((int)BuiltInCategory.OST_NurseCallDevices);
+            categoriesToPurge.Add((int)BuiltInCategory.OST_Parking);
+            categoriesToPurge.Add((int)BuiltInCategory.OST_PipeAccessory);
+            categoriesToPurge.Add((int)BuiltInCategory.OST_PipeFitting);
+            categoriesToPurge.Add((int)BuiltInCategory.OST_Planting);
+            categoriesToPurge.Add((int)BuiltInCategory.OST_PlumbingFixtures);
+            categoriesToPurge.Add((int)BuiltInCategory.OST_RailingSupport);
+            categoriesToPurge.Add((int)BuiltInCategory.OST_RailingTermination);
+            categoriesToPurge.Add((int)BuiltInCategory.OST_SecurityDevices);
+            categoriesToPurge.Add((int)BuiltInCategory.OST_Site);
+            categoriesToPurge.Add((int)BuiltInCategory.OST_SpecialityEquipment);
+            categoriesToPurge.Add((int)BuiltInCategory.OST_Sprinklers);
+            categoriesToPurge.Add((int)BuiltInCategory.OST_StructuralColumns);
+            categoriesToPurge.Add((int)BuiltInCategory.OST_StructuralFoundation);
+            categoriesToPurge.Add((int)BuiltInCategory.OST_StructuralFraming);
+            categoriesToPurge.Add((int)BuiltInCategory.OST_StructuralStiffener);
+            categoriesToPurge.Add((int)BuiltInCategory.OST_TelephoneDevices);
+            categoriesToPurge.Add((int)BuiltInCategory.OST_Windows);
             #endregion
 
             // warn the user if he wants to proceed
@@ -55,101 +102,152 @@ namespace Entry
             {
                 tg.Start();
 
-                // iterate through all editable families and get the editable ones
-                foreach (Family fam in editableFamilies)
-                {                    
-                    Document famdoc = doc.EditFamily(fam); // get family document
+                // iterate through all project families and get the editable ones
+                foreach (Family fam in new FilteredElementCollector(doc)
+                        .OfClass(typeof(Family))
+                        .Cast<Family>()
+                        .Where(q => categoriesToPurge.Contains(q.FamilyCategory.Id.IntegerValue)))
+                {
+                    // declare dictionary of nested families for results
+                    Dictionary<string, List<string>> DictNestResults = new Dictionary<string, List<string>>();
 
-                    List<ElementId> nestedTypesToPurge = new List<ElementId>(); // list of nested types to purge
-                    List<ElementId> nestedFamiliesToPurge = new List<ElementId>(); // list of nested families to purge
-
-                    #region Look inside the family for types to purge and create "nestedTypesToPurge" list	
-                    foreach (ElementType et in new FilteredElementCollector(famdoc)  // Element Types are non user visible elements that contain instances
-                            .OfClass(typeof(ElementType))
-                            .Cast<ElementType>()
-                            .Where(q => q.Category != null && categoriesToPurge.Contains(q.Category.Id.IntegerValue) && q.FamilyName != "Model Text"))
+                    // filter editable families
+                    if (fam != null && fam.IsInPlace == false && fam.IsEditable == true && fam.Name != "Model Text")
                     {
-                        if (new FilteredElementCollector(famdoc)  // compare current loop ElementType id with all placed instances
-                            .WhereElementIsNotElementType()       // if NOT found matches, add ElementType id to purge list
-                            .Where(q => q.GetTypeId() == et.Id).Count() == 0)
-                        {
-                            nestedTypesToPurge.Add(et.Id);
-                        }
-                    }
-                    #endregion
+                        Document famdoc = doc.EditFamily(fam); // get family document
 
-                    #region PURGE TYPES
-                    if (nestedTypesToPurge != null)
-                    {
-                        using (Transaction t = new Transaction (famdoc, "Purge family types"))
+                        List<ElementId> nestedTypesToPurge = new List<ElementId>(); // list of nested types to purge
+                        List<ElementId> nestedFamiliesToPurge = new List<ElementId>(); // list of nested families to purge
+                        List<string> typeList = new List<string>(); // list with types per nested family
+
+                        #region Look inside the family for types to purge and create "nestedTypesToPurge" list	
+                        foreach (ElementType et in new FilteredElementCollector(famdoc)
+                                .OfClass(typeof(ElementType))
+                                .Cast<ElementType>()
+                                .Where(q => q.Category != null 
+                                           && categoriesToPurge.Contains(q.Category.Id.IntegerValue) 
+                                           && q.FamilyName != "Model Text"))
                         {
-                            t.Start();
-                            foreach (ElementId eId in nestedTypesToPurge)
+                            if (new FilteredElementCollector(famdoc)
+                                .WhereElementIsNotElementType()
+                                .Where(q => q.GetTypeId() == et.Id).Count() == 0)
                             {
-                                st_purgedNestedTypes += "-" + famdoc.GetElement(eId).Name + "\n";
-                                famdoc.Delete(eId);
-                            }
-                            t.Commit();
-                        }
-                    }
-                    #endregion
+                                nestedTypesToPurge.Add(et.Id);
 
-                    #region look inside the family for nested families to purge and create "nestedFamiliesToPurge" list
-                    foreach (Family nesfam in new FilteredElementCollector(famdoc)
-                            .OfClass(typeof(Family))
-                            .Cast<Family>()
-                            .Where(q => categoriesToPurge.Contains(q.FamilyCategory.Id.IntegerValue) && q.Name != ""))
-                    {
-                        if (new FilteredElementCollector(famdoc)
-                            .OfClass(typeof(FamilyInstance))
-                            .Cast<FamilyInstance>()
-                            .Where(q => q.Symbol.Family.Id == nesfam.Id)
-                            .Count() == 0)
-                        {
-                            nestedFamiliesToPurge.Add(nesfam.Id);
-                            familiesToPurge.Add(fam);
-                        }
-                    }
-                    familiesToPurge = familiesToPurge.Distinct().ToList();
-                    #endregion
+                                #region Populate dictionary of results. PHASE 1.
 
-                    #region PURGE FAMILIES
-                    foreach (ElementId eId in nestedFamiliesToPurge)
-                    {
-                        if (famdoc.GetElement(eId) != null)
-                        {
-                            //string familyMother = famdoc.OwnerFamily.Name;
-                            try
-                            {
-                                st_purgedNestedFam += "-" + famdoc.GetElement(eId).Name + "\n";
-                                using (Transaction t = new Transaction(famdoc, "Purge nested families Transaction"))
+                                string parentf = fam.Name;
+                                string nestedf = et.FamilyName;
+                                string typef = et.Name;
+
+                                // does parent family exists?
+                                if (DictResults.ContainsKey(parentf))
                                 {
-                                    t.Start();
-                                    famdoc.Delete(eId);
-                                    t.Commit();
+                                    // does nested family exist? If yes, add type
+                                    if (DictNestResults.ContainsKey(nestedf))
+                                    {
+                                        typeList.Add(typef);                    // update typeList
+                                        DictNestResults[nestedf] = typeList;    // update DictNestResults
+                                        DictResults[parentf] = DictNestResults; // update DictResults
+                                        
+                                    }
+                                    // if not, add nested family, add type
+                                    else
+                                    {
+                                        typeList.Add(typef);                    // add first element to typeList
+                                        DictNestResults.Add(nestedf, typeList); // create new key-value pair
+                                        DictResults[parentf] = DictNestResults; // update DictResults
+                                        
+                                    }
                                 }
-
-                            }
-                            catch (Exception)
-                            {
-
-                            }
-                            //st_purgedNestedFam += famdoc.GetElement(eId).Name + "\n";
-                            //using (Transaction t = new Transaction (famdoc, "Purge nested families Transaction"))
-                            //{
-                            //    t.Start();
-                            //    famdoc.Delete(eId);
-                            //    t.Commit();
-                            //}
+                                // If not, add parent family, add nested family, add type
+                                else
+                                {
+                                    typeList.Add(typef);                        // add first element to typeList
+                                    DictNestResults.Add(nestedf, typeList);     // create new key-value pair in DictNestResults
+                                    DictResults.Add(parentf, DictNestResults);  // create new key-value pair in DictResults
+                                    
+                                }
+                                #endregion
+                            }                            
                         }
+                        #endregion
+
+                        #region PURGE TYPES
+                        if (nestedTypesToPurge != null)
+                        {
+                            using (Transaction t = new Transaction (famdoc, "Purge family types"))
+                            {
+                                t.Start();
+                                foreach (ElementId eId in nestedTypesToPurge)
+                                {
+                                    lt_purgedNestedTypes.Add(famdoc.GetElement(eId).Name);
+                                    st_purgedNestedTypes += "-" + famdoc.GetElement(eId).Name + "\n";
+                                    famdoc.Delete(eId);
+                                }
+                                t.Commit();
+                            }
+                        }
+                        #endregion
+
+                        #region look inside the family for nested families to purge and create "nestedFamiliesToPurge" list
+                        foreach (Family nesfam in new FilteredElementCollector(famdoc)
+                                .OfClass(typeof(Family))
+                                .Cast<Family>()
+                                .Where(q => categoriesToPurge.Contains(q.FamilyCategory.Id.IntegerValue) && q.Name != ""))
+                        {
+                            if (new FilteredElementCollector(famdoc)
+                                .OfClass(typeof(FamilyInstance))
+                                .Cast<FamilyInstance>()
+                                .Where(q => q.Symbol.Family.Id == nesfam.Id)
+                                .Count() == 0)
+                            {
+                                nestedFamiliesToPurge.Add(nesfam.Id);
+                                familiesToPurge.Add(fam);
+                            }
+                        }
+                        familiesToPurge = familiesToPurge.Distinct().ToList();
+                        #endregion
+
+                        #region PURGE FAMILIES
+                        foreach (ElementId eId in nestedFamiliesToPurge)
+                        {
+                            if (famdoc.GetElement(eId) != null)
+                            {
+                                //string familyMother = famdoc.OwnerFamily.Name;
+                                try
+                                {
+                                    lt_purgedNestedFam.Add(famdoc.GetElement(eId).Name);
+                                    st_purgedNestedFam += "-" + famdoc.GetElement(eId).Name + "\n";
+                                    using (Transaction t = new Transaction(famdoc, "Purge nested families Transaction"))
+                                    {
+                                        t.Start();
+                                        famdoc.Delete(eId);
+                                        t.Commit();
+                                    }
+
+                                }
+                                catch (Exception)
+                                {
+
+                                }
+                                //st_purgedNestedFam += famdoc.GetElement(eId).Name + "\n";
+                                //using (Transaction t = new Transaction (famdoc, "Purge nested families Transaction"))
+                                //{
+                                //    t.Start();
+                                //    famdoc.Delete(eId);
+                                //    t.Commit();
+                                //}
+                            }
+                        }
+                        #endregion
                     }
-                    #endregion
-                    
                 }
 
                 #region Load families back to the project
                 foreach (Family f in familiesToPurge)
                 {
+                    lt_purgedFamilies.Add(f.Name);
                     st_purgedFamilies += "-" + f.Name + "\n";
                     Document fdoc = doc.EditFamily(f);
 
@@ -165,9 +263,19 @@ namespace Entry
             TaskDialog.Show("Information", "Purged Families:" + "\n" + st_purgedFamilies + "\n"
                                            + "Deleted Nested Families:" + "\n" + st_purgedNestedFam + "\n"
                                            + "Deleted Nested Types:" + "\n" + st_purgedNestedTypes);
+
+            using (UI.Form_Results thisForm = new UI.Form_Results())
+            {
+                thisForm.ShowDialog();
+                if (thisForm.DialogResult == System.Windows.Forms.DialogResult.Cancel)
+                {
+                    return Result.Cancelled;
+                }
+            }
+
             try
             {
-                Data.Helpers.GetAnalyticsCSV(doc, app);
+                Utilities.GetAnalyticsCSV(doc, app);
             }
             catch (Exception)
             {
---
+++ 2020/PurgeNestedFamilies/VS_PurgeNestedFamilies/Form/Form_Results.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI
+{
+    using static Entry.CmdMain;
+
+    public partial class Form_Results : System.Windows.Forms.Form
+    {
+        public Form_Results()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_Results_Load(object sender, EventArgs e)
+        {
+            LoadTree();
+            Dictionary<string, List<string>> somedict = DictResults.FirstOrDefault().Value;
+            label1.Text = somedict.FirstOrDefault().Value.FirstOrDefault();
+        }
+
+        #region HELPERS
+        private void LoadTree()
+        {
+            TreeResults.Nodes.Clear();
+
+            foreach (KeyValuePair<string, Dictionary<string, List<string>>> parentf in DictResults)
+            {
+                // add parent family
+                TreeResults.Nodes.Add(parentf.Key, parentf.Key);
+
+                // loop through nested families
+                foreach (KeyValuePair<string, List<string>> nestedf in parentf.Value)
+                {
+                    TreeResults.Nodes[parentf.Key].Nodes.Add(nestedf.Key, nestedf.Key);
+
+                    foreach (string type in nestedf.Value)
+                    {
+                        if (type != null)
+                        {
+                            TreeResults.Nodes[parentf.Key].Nodes[nestedf.Key].Nodes.Add(type);
+                        }                       
+                    }
+                }
+            }
+        }
+        #endregion
+    }
+}
